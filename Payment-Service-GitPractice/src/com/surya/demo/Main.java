359a2dea681ec3f08c759ee103f4691edf5efdd6
surya-narayan-swain <surya.swain2018@gmail.com>
2025-10-07 01:01:48 +0530

    SortArray class is added

--- Payment-Service-GitPractice/src/com/surya/demo/Main.java
+++ Payment-Service-GitPractice/src/com/surya/demo/Main.java
@@ -1,15 +1,23 @@
 package com.surya.demo;
 
+import java.util.Arrays;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("Main.main()");
 		System.out.println("Payment-service");
 		System.out.println("=======================");
 		System.out.println("welcome");
 		System.out.println("github");
- String str=StringReverse.reverse("SURYA");
- System.out.println(str);
+		String str = StringReverse.reverse("SURYA");
+		System.out.println(str);
+		System.out.println("================");
+		int arr[] = { 5, 2, 1, 4, 3, 4 };
+		System.out.println(Arrays.toString(arr));
+		SortArray.sortArr(arr);
+		System.out.println(Arrays.toString(arr));
+
 	}
 }
