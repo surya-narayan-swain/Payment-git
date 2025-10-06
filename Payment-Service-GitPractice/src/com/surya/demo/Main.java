486b57947cf5c3a26740cc8fcdb4a4d329b7449b
surya-narayan-swain <surya.swain2018@gmail.com>
2025-10-06 20:58:14 +0530

    Initial commit

---
+++ Payment-Service-GitPractice/src/com/surya/demo/Main.java
@@ -0,0 +1,10 @@
+package com.surya.demo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Main.main()");
+		System.out.println("Payment-service");
+	}
+}
